8ebbf438525a7690f4bbf5b2c0e5a8d3d8494a81
marloikhodi <deivid.serafim03@gmail.com>
2025-09-19 16:57:36 -0300

    rotas

--- nodejs/aulas/01-fundamento-nodejs/src/server.js
+++ nodejs/aulas/01-fundamento-nodejs/src/server.js
@@ -1,7 +1,28 @@
-import http from "node:http";
+import http from "node:http"; //"node:" informa que o pacote é nativo do node
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// métodos principais:
+// GET, POST, PUT, PATCH, DELETE
+// São utilizado de maneira semântica e não funcional (significado)
+
+// GET /users => Buscando um usuário no backend
+// POST /users => Criando um usuário no backend
 
 const server = http.createServer((req, res) => {
   // request e response
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuários");
+  }
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuário");
+  }
+
+  // console.log(method, url);
   return res.end("Hello World");
 });
 
