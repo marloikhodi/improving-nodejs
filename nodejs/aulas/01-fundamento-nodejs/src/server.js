d1c4e5133b7e58f0e32761772ee97d367a5a3d54
marloikhodi <deivid.serafim03@gmail.com>
2025-09-20 00:45:43 -0300

    headers e metodo POST

--- nodejs/aulas/01-fundamento-nodejs/src/server.js
+++ nodejs/aulas/01-fundamento-nodejs/src/server.js
@@ -11,14 +11,26 @@ import http from "node:http"; //"node:" informa que o pacote é nativo do node
 // GET /users => Buscando um usuário no backend
 // POST /users => Criando um usuário no backend
 
+// Stateful = dados armazenado em memoria no node
+// Stateless = dados armazenador em terceiro (banco de dados)
+
+// Cabeçalhos (req,res) => Metadados (não necessariamente informações)
+const users = [];
+
 const server = http.createServer((req, res) => {
   // request e response
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    return res.setHeader("Content-type", "application/json").end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: users.length + 1,
+      nome: "John Doe",
+      email: "johndoe@example.com",
+    });
+
     return res.end("Criação de usuário");
   }
 
