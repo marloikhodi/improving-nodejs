e83e9d660adab0d5519db2f4ca2fca4f36d27823
marloikhodi <deivid.serafim03@gmail.com>
2025-09-24 12:13:04 -0300

    middlewares

--- nodejs/aulas/01-fundamento-nodejs/src/server.js
+++ nodejs/aulas/01-fundamento-nodejs/src/server.js
@@ -1,49 +1,18 @@
-import http from "node:http"; //"node:" informa que o pacote é nativo do node
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// métodos principais:
-// GET, POST, PUT, PATCH, DELETE
-// São utilizado de maneira semântica e não funcional (significado)
-
-// GET /users => Buscando um usuário no backend
-// POST /users => Criando um usuário no backend
-
-// Stateful = dados armazenado em memoria no node
-// Stateless = dados armazenador em terceiro (banco de dados)
-
-// Cabeçalhos (req,res) => Metadados (não necessariamente informações)
-
-// HTTP Status Code
-// Códigos númericos (semantica) para dizer ao front-end a situação da requisição(se deu erro, se está ok, se foi falta de info, etc)
+import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 const users = [];
 
 const server = http.createServer(async (req, res) => {
-  // request e response
-  const { method, url } = req; // métodos sendo GET, POST, etc... & url sendo o caminho da requisiçao (/users por exemplo)
-  const buffers = []
+  const { method, url } = req;
 
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-    //JSON.parse transforma em JSON ao enviar
-  } catch {
-    res.body = null
-  }
+  await json(req, res);
 
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users)); //o return deve ser em JSON principalmente
+    return res.end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
-    const { nome, email } = req.body
+    const { nome, email } = req.body;
 
     users.push({
       id: users.length + 1,
@@ -51,17 +20,10 @@ const server = http.createServer(async (req, res) => {
       email,
     });
 
-    return res
-      .writeHead(201)
-      .end('Usuário Criado');
+    return res.writeHead(201).end("Usuário Criado");
   }
 
-  // console.log(method, url);
-  return res
-    .writeHead(404)
-    .end("Not Found");
+  return res.writeHead(404).end("Not Found");
 });
 
 server.listen(3333);
-
-// localhost:3333
