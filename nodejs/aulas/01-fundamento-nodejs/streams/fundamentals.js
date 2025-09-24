3c75ff61b7c9d93fe443499117670a5a5175911f
marloikhodi <deivid.serafim03@gmail.com>
2025-09-24 09:38:47 -0300

    streams

--- nodejs/aulas/01-fundamento-nodejs/streams/fundamentals.js
+++ nodejs/aulas/01-fundamento-nodejs/streams/fundamentals.js
@@ -1,6 +1,6 @@
 // Apliocativos de Steam : netflix e spotify
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 // Streams ->
 // process.stdin
@@ -13,14 +13,34 @@ class oneToHundredStream extends Readable {
         setTimeout(() => {
 
             if (i > 100) {
-                this.push(null) //.push é o metodo de uma readable stream fornecer dados para quem está a consumindo 
+                this.push(null)
+                //.push é o metodo de uma readable stream fornecer dados para quem está a consumindo 
                 // null é o fim da stream
             } else {
                 const buff = Buffer.from(String(i))
                 this.push(buff)
+                //tudo enviado da stream de leitura é um CHUNK
             }
-        }, 1000);
+        }, 500);
     }
 }
 
-new oneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(transformed)))
+        // primeiro parametro do callback é um erro, segundo parametro é o valor
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    //stream de escrita somente PROCESSA o dado, nao transforma
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new oneToHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
