75b554279533f49791f74cf8cd0b70fbf33a7587
marloikhodi <deivid.serafim03@gmail.com>
2025-09-24 11:41:34 -0300

    buffer

---
+++ nodejs/aulas/01-fundamento-nodejs/streams/buffer.js
@@ -0,0 +1,8 @@
+// Espaço na memoria do computador, utilziado para movimentar dados de maneira muito perfomatica
+// De maneira temporaria - excluido após utilizado
+
+const buf = Buffer.from('Hello')
+
+console.log(buf)
+
+console.log(buf.toJSON())
