02cfd50de5014308484151455b00fcb64d8533ae
marloikhodi <deivid.serafim03@gmail.com>
2025-09-30 12:14:31 -0300

    atualização de registros

--- nodejs/01-fundamento-nodejs/src/util/build-route-path.js
+++ nodejs/01-fundamento-nodejs/src/util/build-route-path.js
@@ -3,6 +3,6 @@ export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
   const pathWithParams = path.replaceAll(routeParametersRegex, "(?<$1>[-a-z0-9_]+)");
 
-  const pathRegex = new RegExp(`^${pathWithParams}`);
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
   return pathRegex;
 }
