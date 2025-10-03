0d2d3f667008c69ba259ea1edce14e759ee2c8df
marloikhodi <deivid.serafim03@gmail.com>
2025-10-03 20:08:32 -0300

    criando rotas com o knex

--- nodejs/02-api-rest-nodejs/src/server.ts
+++ nodejs/02-api-rest-nodejs/src/server.ts
@@ -1,12 +1,26 @@
+import crypto from 'node:crypto'
 import fastify from 'fastify'
 import { knex } from './database.js'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-	const tables = await knex('sqlite_schema').select('*')
+	const transaction = await knex('transactions')
+		.insert({
+			id: crypto.randomUUID(),
+			title: 'Transação de teste',
+			amount: 500,
+		})
+		.returning('*')
 
-	return tables
+	return transaction
+})
+app.get('/world', async () => {
+	const transaction = await knex('transactions')
+	.where('amount', 500)
+	.select('*')
+
+	return transaction
 })
 
 app
