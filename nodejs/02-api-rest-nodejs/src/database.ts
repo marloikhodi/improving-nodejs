dce910f1b7a28863c5a5f99e810a6673a7b9c371
marloikhodi <deivid.serafim03@gmail.com>
2025-10-04 23:01:00 -0300

    correção nas migrations

--- nodejs/02-api-rest-nodejs/src/database.ts
+++ nodejs/02-api-rest-nodejs/src/database.ts
@@ -4,12 +4,12 @@ import { env } from '../env/index.js'
 export const config: Knex.Config = {
 	client: 'sqlite3',
 	connection: {
-		filename: './db/app.db',
+		filename: env.DATABASE_URL,
 	},
 	useNullAsDefault: true,
 	migrations: {
 		extension: 'ts',
-		directory: env.DATABASE_URL,
+		directory: './db/migrations',
 	},
 }
 
