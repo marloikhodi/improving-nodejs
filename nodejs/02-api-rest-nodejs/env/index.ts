39f6225ee8432176b81d6ffbd794946221079f74
marloikhodi <deivid.serafim03@gmail.com>
2025-10-06 22:05:57 -0300

    requisitos da aplicação

--- nodejs/02-api-rest-nodejs/env/index.ts
+++ nodejs/02-api-rest-nodejs/env/index.ts
@@ -7,17 +7,17 @@ import { z } from 'zod'
 
 const envSchema = z.object({
 	//.object pois o process.env retorna um objeto
-	NODE_ENV: z.enum(['development','test','production']).default('production'), //enum -> uma entre
+	NODE_ENV: z.enum(['development', 'test', 'production']).default('production'), //enum -> uma entre
 	DATABASE_URL: z.string(), //.nullable() caso nao fosse um valor obrigatorio
 	PORT: z.number().default(3333),
 })
 
- const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env)
 
- if (_env.success === false) {
+if (_env.success === false) {
 	console.error('Invalid Enviroment Variables', _env.error.format())
 
 	throw new Error('Invalid enviroment variables.')
- }
+}
 
- export const env = _env.data
+export const env = _env.data
---
+++ nodejs/02-api-rest-nodejs/requisitos-da-aplicacao.md
@@ -0,0 +1,18 @@
+## Aplicação de Controle de Finanças Pessoal
+
+# RF - Requisitos Funcionais
+
+[ ] - O usuário deve poder criar uma nova transação;
+[ ] - O usuário deve poder obter um resumo da sua conta;
+[ ] - O usuário deve poder listar todas as transações que já ocorreram;
+[ ] - O usuário deve poder visualizar uma transação única;
+
+# RN - Regras de Negocio
+
+[ ] - A transação pode ser do tipo crédito que somará ao valor total, ou débito que subtrairá;
+[ ] - Deve ser possível indentificarmos o usuário entre as requisições
+[ ] - O usuário só pode visualizar transações o qual ele criou;
+
+# RNF - Requisitos não funcionais
+
+-
