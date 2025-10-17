bc0c0a9091651716acd1d688962a437711c69e04
marloikhodi <deivid.serafim03@gmail.com>
2025-10-17 16:00:38 -0300

    nomeando testes

---
+++ nodejs/02-api-rest-nodejs/test/transactions.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import { app } from '../src/app.js'
+
+describe('Transactions routes', () => {
+	// maneira de nomear o teste, possivel inserir subcategorias da mesma maneira dentro de outra
+	beforeAll(async () => {
+		await app.ready() // abre o servidor antes de tudo
+	})
+
+	afterAll(async () => {
+		await app.close() // fecha o servidor depois de tudo
+	})
+
+	it('sould be able to create a new transition', async () => {
+		await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'New Transaction',
+				amount: 5000,
+				type: 'credit',
+			})
+			.expect(201)
+	})
+})
