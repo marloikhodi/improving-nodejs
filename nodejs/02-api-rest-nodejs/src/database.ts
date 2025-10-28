c88ec3cb8872d4eba0b38080911a9cc5838d718a
marloikhodi <deivid.serafim03@gmail.com>
2025-10-28 19:31:32 -0300

    debugging

--- nodejs/02-api-rest-nodejs/src/database.ts
+++ nodejs/02-api-rest-nodejs/src/database.ts
@@ -2,8 +2,8 @@ import setupKnex, { type Knex } from 'knex'
 import { env } from '../env/index.js'
 
 export const config: Knex.Config = {
-	client: env.DATABSE_CLIENT,
-	connection: env.DATABSE_CLIENT === 'sqlite' ?{
+	client: env.DATABASE_CLIENT,
+	connection: env.DATABASE_CLIENT === 'sqlite' ?{
 		filename: env.DATABASE_URL,
 	} : env.DATABASE_URL,
 	useNullAsDefault: true,
